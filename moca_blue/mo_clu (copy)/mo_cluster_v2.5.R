2e09e0ea18d7d408d9e384370c167461e96240a6
SMZuckerle <110096442+SMZuckerle@users.noreply.github.com>
2024-03-13 16:35:46 +0100

    Add files via upload

---
+++ moca_blue/mo_clu (copy)/mo_cluster_v2.5.R
@@ -0,0 +1,169 @@
+#mo_cluster
+# DOCU:
+# This script is used to cluster motifs stored a weight matrices based on the Smith Waterman, Sandelin Wassermann (2004) or euclidian distance methods 
+# and results are obtained as Newick tree (NWK tree) representation of the clustered motifs. 
+# Additionally, it identifies sets of motifs with high similarity and separates them into two categories: 
+# Motifs with highly similar counterparts and motifs without highly similar counterparts. 
+# Below is the more detailed documentation for scripts modules:
+#
+# SETUP:
+# The script starts by setting up some variables and libraries required for motif clustering.
+# NAME0: Name identifier for source file, SPEC: Specifies the species (e.g., "Zema"), MODEL: Specifies the model (e.g., "S0", "S1" or "S0+M0"), TYPE: Specifies the type of motif file (e.g., "_cwm-motifs.jaspar")
+# To run and store the script the parent directory requires a daugther directory named "out" where the output is stored and other scripts of the moca_blue suite will access it.
+# FILEPATHs: 
+# FILE1: Combines the setup variables to create the input motif file path, dirpath_in: Specifies the input directory path, dirpath_out: Specifies the output directory path.
+# LIBARY DEPENDENCIES: 
+# The script loads several R libraries including grid, TFBSTools, motifStack, universalmotif, ape, and ggtree.
+# 
+# PROCESSING STEPS:
+# [1] - Motifs are read from the JASPAR motif file specified in FILE1 using the read_jaspar function. The motifs are stored in the cwm1 list.
+# [2] - Motif names are modified to include additional information. The script extracts the number of sites (nsites or consensus) from the motif name and appends it to the motif name.
+# [3] - The motifs are converted into different formats, such as position weight matrices (PWM) and position count matrices (PCM) for later analyses depending on the clustering algorithm.
+# [4] - The script summarizes the motifs in PCM format and writes the summary to a CSV file.
+# [5] - Motifs are clustered using a selected method using the universalmotif library. Motifs can be compared using e.g. Sandelin Wassermann algorithm ("SW") or others. (See documentation https://bioconductor.org/packages/release/bioc/html/universalmotif.html)
+# [6] - Highly similar motifs are identified by comparing the clustering results. Motifs that have highly similar counterparts are separated from those that don't.
+# [6.1] The script also performs clustering using the Smith-Waterman method and saves the resulting NWK tree.
+# [7] - From the clustering results, Highly similar (using the upper 5 percent quantile) motifs are identified . Motifs that have highly similar counterparts are separated from those that don't.
+#       The comparison matrix, used here, is converted into a data frame. This data frame contains the motif similarity scores, with motifs as rows and columns. 
+#       The quantile function is used to calculate the 5th percentile of the similarity scores in the data frame. 
+#       This value represents a lower threshold for considering motifs as highly similar.
+# [8] - Various outputs are written to files, including the NWK tree of clustered motifs, lists of motifs with and without highly similar counterparts, and a matrix of motif comparisons.
+
+# 2023-10-05 by Dr. Simon M. Zumkeller
+######################################################
+################## [SETUP] ###########################
+setwd("/home/ibg-4/Desktop/Rhome/moca_blue/mo_clu")
+######################################################
+NAME0="rdf5_12epm"
+SPEC ="At-Sb-Sl-Zm"
+MODEL="M0S0" # C0 stand for DeepCistrome version 1 (available at 02-may-2023) Standard conditions
+TYPE ="_cwm-motifs.jaspar"
+#######################################################
+#FILE1 = paste0(NAME0,SPEC,MODEL,TYPE)
+FILE1 = "rdf5_12At-Sb-Sl-ZmM0+S0_cwm-motifs.jaspar"
+#######################################################
+folder_name <- paste0(SPEC, "_", MODEL)
+dir.create(folder_name, showWarnings = FALSE)
+#######################################################
+dirpath_in = "../mo_nom/out/"
+dirpath_out = "./out/"
+setwd("/home/ibg-4/Desktop/Rhome/moca_blue/mo_clu") 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+File1 <- paste0(dirpath_in,FILE1)
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+library(grid)
+library(TFBSTools)
+library(motifStack)
+library(universalmotif)
+library(ape)
+library(ggtree)
+library(ggplot2)
+library(ggseqlogo)
+library(Cairo)
+# ##################################################
+cwm1 <- read_jaspar(File1)
+##################################################
+# Loop through each motif object in the list
+for (i in seq_along(cwm1)) {
+  # Extract the motif name and the number after the last "_" underscore
+  motif_name <- attr(cwm1[[i]], "name")
+  nsites <- as.numeric(sub(".+_(\\d+)$", "\\1", motif_name))
+  # Assign the nsites value to the "nsites" field of the motif object
+  cwm1[[i]]["nsites"] <- nsites
+}
+##################################################
+# Loop through each motif object in the list
+for (i in seq_along(cwm1)) {
+  # Extract the Total IC and the Consensus values from the motif object
+  total_ic <- attr(cwm1[[i]], "icscore")
+  total_ic_rounded <- round(total_ic, 1)
+  consensus <- attr(cwm1[[i]], "consensus")
+  # Combine the Total IC and the Consensus values separated by "_" to the motif name
+  motif_name <- attr(cwm1[[i]], "name")
+  new_motif_name <- paste0(motif_name, "_", total_ic_rounded, "_", consensus)
+  # Assign the new motif name to the motif object
+  attr(cwm1[[i]], "name") <- new_motif_name
+}
+##################################################
+pwm_uni0<-convert_motifs(
+  cwm1, class = "TFBSTools-PWMatrix")
+pcm<-convert_motifs(
+  cwm1, class = "motifStack-pcm")
+##################################################
+#ggseqlogo(pcm[[1]]@mat)
+#pcm[[1]]@name
+# Your existing code to generate the sequence logo
+#seq_logo <- ggseqlogo(pcm[[1]]@mat)
+#seq_logo <- seq_logo + theme_minimal() + theme(
+#  plot.background = element_rect(fill = "white"),
+#  panel.background = element_rect(fill = "white"),
+#  legend.background = element_rect(fill = "white")
+# Save the plot with custom width and height
+#ggsave("your_plot.png", plot = seq_logo, width = 4831, height = 592, units = "px")
+pcm_length <- length(pcm)  # Getting the length of the pcm object
+for (i in 1:pcm_length) {
+  seq_logo <- ggseqlogo(pcm[[i]]@mat)
+  seq_logo <- seq_logo + theme_minimal() + theme(
+    plot.background = element_rect(fill = "white"),
+    panel.background = element_rect(fill = "white"),
+    legend.background = element_rect(fill = "white")
+  )
+  # Save the plot with custom width and height
+  file_name <- paste0(pcm[[i]]@name, ".png")
+  file_path <- file.path(folder_name, file_name)
+  ggsave(file_path, plot = seq_logo, width = 4831, height = 592, units = "px")
+}
+##################################################
+sum<-as.data.frame(summarise_motifs(pcm))
+write.csv(sum, file = paste0(dirpath_out,FILE1,"summary.txt"))
+##################################################
+##########################################################
+c<-compare_motifs(cwm1, method = "SW")
+c0<-as.data.frame(c)
+c1 <- as.matrix(c0)
+lower_percentile <- quantile(c1, probs = 0.05)
+upper_percentile <- quantile(c1, probs = 0.95)
+##############         ####################      ##################
+##############         ####################      ##################
+c2 <- c0-upper_percentile
+c3 <- as.data.frame(ifelse(c2 < 0, 0, 1))
+c3$sum <- rowSums(c3)
+epms_without_highly_similar_counterparts <- rownames(c3[c3$sum == 2, ])
+epms_with_highly_similar_counterparts <- rownames(c3[c3$sum != 2, ])
+write.table(epms_without_highly_similar_counterparts, file = paste0(dirpath_out, FILE1, "_epms_without_highly_similar_counterparts-SW.csv"), sep = "\t", col.names = NA, quote = FALSE)
+write.table(epms_with_highly_similar_counterparts, file = paste0(dirpath_out, FILE1, "_epms_with_highly_similar_counterparts-SW.csv"), sep = "\t", col.names = NA, quote = FALSE)
+write.table(c0, file = paste0(dirpath_out, FILE1, "_matrix-SW.csv"), sep = "\t", col.names = NA, quote = FALSE)
+#assign scores from data.frame to branches for selection
+comp_1 <- 1-c
+comp_1 <- as.dist(comp_1)
+#labels <- attr(comp_1, "Labels")
+comp_2 <- ape::as.phylo(hclust(comp_1))
+comp_2[["edge.length"]] <- comp_2[["edge.length"]]+1
+comp_2[["edge.length"]]
+# Create a rooted phylo object
+phylo_tree <- as.phylo(comp_2)
+# Save the tree with positive edge lengths
+write.tree(comp_2, file = paste0(dirpath_out, FILE1, "-Sandelin-Wassermann.nwk"))
+#################################################
+c_pcm<-clusterMotifs(pcm, method = "Smith-Waterman") ### !!! TIME TO GET A COFFEEE !!! ###
+hc<- c_pcm
+motifs<-pcm[hc$order]
+##################################################
+write.tree(as.phylo(c_pcm), file = paste0(dirpath_out,FILE1,"-Smith-Waterman.nwk"))
+
+#par(cex = 0.2)
+#pdf("output3.pdf", width = 30, height = 40)
+#plotMotifLogoStackWithTree(pcm, c_pcm,
+#                           treewidth = 1/8,
+#                           trueDist = TRUE)
+#dev.off()
+
+browseMotifs(pcm,
+             layout = c("tree"),
+             nodeRadius = 3,
+             baseHeight = 30,
+             baseWidth = 12,
+             width = 100,
+             height = 2500)
+
+
