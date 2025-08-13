55ba67950999cf20d26686d0611208ebc4a3338f
Gernot Schmitz <spammail1240@web.de>
2025-08-13 09:40:30 +0200

    occ_filter_v1.1.R takes input parameters now changed logic to work with already extracted gene flanking regions

--- moca_blue/mo_proj/occ_filter_v1.1.R
+++ moca_blue/mo_proj/occ_filter_v1.1.R
@@ -11,25 +11,53 @@
 #split -l $outputSize --numeric-suffixes $inputFile smallfile
 #mkdir occ$project
 #mv smallfile* occ$project/
-#############################################################
-
-
-setwd("/home/ibg-4/Desktop/Rhome/moca_blue/mo_proj")
-
-
 ############################################################# ITS WORKING !
+working_directory = "/home/ibg-4/Desktop/Rhome/moca_blue/mo_proj"
+file_paths = "../mapping/msr_arabidopsis/occurrences.txt"
+output_file <- "../out/SolyM014sols_e3-cwm_2023110"
 dirpath <- "./occSolyM014sols_e3-cwm_20231110/"
-output_file <- "./out/SolyM014sols_e3-cwm_2023110"
-file_paths  <- list.files(dirpath, pattern = "^smallfile*", full.names = TRUE)
+# file_paths  <- list.files(dirpath, pattern = "^smallfile*", full.names = TRUE)
+#############################################################
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) > 4) {
+  stop("Cannot provide more than 4 arguments! Usage: <script> [<working_directory>] [<file_paths>] [<output_file>] [<dirpath>]", call.=FALSE)
+}
+if (length(args)>=4) {
+  dirpath = args[4]
+}
+if (length(args)>=3) {
+  output_file = args[3]
+}
+if (length(args)>=2) {
+  file_paths = args[2]
+}
+if (length(args)>=1) {
+  working_directory = args[1]
+}
+
+if (file.exists(file_paths)){
+  file_paths = c(file_paths)
+} else if (dir.exists(dirpath)) {
+  # list all files in the directory that match the pattern
+  file_paths <- list.files(dirpath, pattern = "^smallfile.*\\.txt$", full.names = TRUE)
+} else {
+  stop("No valid file or directory found.")
+}
+
+# print all arguments
+print(paste("directory path:", dirpath))
+print(paste("Output file:", output_file))
+print(paste("working directory:", working_directory))
+print(paste("File paths:", paste(file_paths, collapse = ", ")))
 ############################################################# 
 library(tidyr)
 library(dplyr)
 library(readr)
 library(magrittr)
 ############################################################# 
+setwd(working_directory)
 # initialize an empty data frame to store the results
 combined_df <- data.frame()
-
 # loop over all files and append the results to the combined data frame
 for (filepath in file_paths) {
   # read the file and skip lines with less than 9 elements
@@ -46,9 +74,40 @@ for (filepath in file_paths) {
                                "V7",
                                "V8")
   
+
+
+
+
+
+
+
+
+
   # split the "loc" column into "chr", "gene_start", and "gene_end"
  # selected_df <- separate(occurrence_df, loc, into = c("chr", "gene_start", "gene_end"), sep = "[:-]", fill = "left")
-  selected_df <- separate(occurrence_df,
+  # selected_df <- separate(occurrence_df,
+  #                         loc,
+  #                         into = c("chr", "gene_loc"),
+  #                         sep = ":",
+  #                         fill = "left")  %>%
+  #   separate(
+  #     gene_loc, into = c("gene_start", "gene_end"),
+  #     sep = "-",
+  #     fill = "left")
+  
+  # # combine "chr", "gene_start", and "gene_end" into a new "loc" column
+  # selected_df$loc <- paste(selected_df$chr,
+  #                          selected_df$gene_start,
+  #                          sep = ":")
+  # selected_df$loc <- ifelse(
+  #   is.na(
+  #     selected_df$gene_end),
+  #   selected_df$loc,
+  #   paste(
+  #     selected_df$loc,
+  #     selected_df$gene_end,
+  #     sep = "-"))
+selected_df <- separate(occurrence_df,
                           loc,
                           into = c("chr", "gene_loc"),
                           sep = ":",
@@ -58,18 +117,35 @@ for (filepath in file_paths) {
       sep = "-",
       fill = "left")
   
-  # combine "chr", "gene_start", and "gene_end" into a new "loc" column
-  selected_df$loc <- paste(selected_df$chr,
-                           selected_df$gene_start,
-                           sep = ":")
-  selected_df$loc <- ifelse(
-    is.na(
-      selected_df$gene_end),
+# combine "chr", "gene_start", and "gene_end" into a new "loc" column
+selected_df$loc <- ifelse(
+  any(is.na(selected_df$chr), is.na(selected_df$gene_start)),
+  selected_df$gene_end,
+  paste(selected_df$chr, selected_df$gene_start, sep = ":")
+)
+
+selected_df$loc <- ifelse(
+  is.na(selected_df$gene_start),
+  selected_df$loc,
+  paste(
     selected_df$loc,
-    paste(
-      selected_df$loc,
-      selected_df$gene_end,
-      sep = "-"))
+    selected_df$gene_end,
+    sep = "-"))
+selected_df$gene_start = 1001
+selected_df$gene_end = 2020
+
+
+
+
+
+
+
+
+
+
+
+
+
   
   # filter rows based on the specified condition
   filtered_df <- selected_df %>%
@@ -80,10 +156,10 @@ for (filepath in file_paths) {
   # append the filtered data frame to the combined data frame
   combined_df <- rbind(combined_df, filtered_df)
 }
-
+# create folders to save the output
+dir.create(dirname(output_file), recursive = TRUE, showWarnings = FALSE)
 # write the combined data frame to a file
 write.table(combined_df, 
             output_file, 
             sep = "\t",
             row.names = FALSE)
-
