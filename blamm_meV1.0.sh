9364aa46d8c06a071c0951f98b9b84018d201b85
Gernot Schmitz <spammail1240@web.de>
2025-08-13 09:38:12 +0200

    updated blamm script to take input parameters

--- blamm_meV1.0.sh
+++ blamm_meV1.0.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
 # Define variables
-project="Spen_ch01_0e3"
+out_dir="random_sequences"
+motifs_file="out/rdf5_MSR_ARTHM1X0.75dC7K25f_cwm-motifs.jaspar"
+sequences="ref_seq/sequences_random.mf"
 pt_score=0.0001
-motifs_file="Soly-MSR_20230118_uu.jaspar"
+
+
+# Parse input arguments
+if [ $# -gt 5 ]; then
+  echo "Usage: $0 [out_dir] [motifs_file] [sequences] [pt_score]"
+  exit 1
+fi
+if [ $# -ge 1 ]; then
+  out_dir="$1"
+fi
+if [ $# -ge 2 ]; then
+  motifs_file="$2"
+fi
+if [ $# -ge 3 ]; then
+  sequences="$3"
+fi
+if [ $# -ge 4 ]; then
+  pt_score="$4"
+fi
 
 #
 # Start recording runtime and resource information
@@ -12,17 +32,17 @@ start_resources=$(ps -o pid,%cpu,%mem,vsz,rss,tty,stat,start_time --no-headers $
 
 
 # Run the commands with variables
-./blamm dict sequences.mf
-./blamm hist -e "$motifs_file" sequences.mf #-e generated empirical PWM scores
-./blamm scan -rc -pt "$pt_score" "$motifs_file" sequences.mf
+blamm dict "$sequences"
+blamm hist -e "$motifs_file" "$sequences" #-e generated empirical PWM scores
+blamm scan -rc -pt "$pt_score" "$motifs_file" "$sequences"
 
 #
 echo "CLEANING UP"
 
-mkdir occ$project
-mv hist_* occ$project/
-mv occurrences.txt occ$project/
-mv PWMthresholds.txt occ$project/
+mkdir -p $out_dir/histograms
+mv hist_* $out_dir/histograms/
+mv occurrences.txt $out_dir/
+mv PWMthresholds.txt $out_dir/
 
 #
 # Stop recording runtime and resource information
